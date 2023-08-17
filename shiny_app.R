b08282d3197ab2cab9aa0960f15665b74072d0e3
juliablanchard <julia.blanchard@utas.edu.au>
2023-08-17 17:18:52 +1000

    a rudimentray attempt at shiny

--- shiny_app.R
+++ shiny_app.R
@@ -18,98 +18,135 @@ library(metR)
 library(lubridate)
 library(raster)
 library(sf)
+library(terra)
 
-#file="http://portal.sf.utas.edu.au/thredds/catalog/gem/fishmip/ISIMIP3a/InputData/fishing/histsoc/catalog.html?dataset=fishmip/ISIMIP3a/InputData/fishing/histsoc/calibration_catch_histsoc_1850_2004.csv"
-#fishmip <- read_csv(file="http://portal.sf.utas.edu.au/thredds/fileServer/gem/fishmip/ISIMIP3a/InputData/fishing/histsoc/calibration_catch_histsoc_1850_2004.csv")
+# get the mask for regional ecosystem models
+mask <- read.csv(file="Masks_netcdf_csv/fishMIP_regional_1deg_ISIMIP3a.csv")
+regions<-unique(mask$region)
 
-fishmip <- read.csv(file="EEZ_tcb.csv")
-fishmip <- fishmip[fishmip$dataType=="per",]
-#fishmip <- fishmip[fishmip$model=="BOATS",]
-fishmip <- fishmip[fishmip$value !="Inf",]
-
-
-#multimodel mean & sd
-fishmip<-fishmip %>% group_by(year,EEZ,ssp) %>%
-  summarise(mean = mean(value), sd = sd(value),median=median(value),min=min(value),max=max(value))
-
-  
-fishmip$EEZ<-as.factor(fishmip$EEZ)
-# fishmip$model<-as.factor(fishmip$model)
-# fishmip$esm<-as.factor(fishmip$esm)
-
-fishmip$ssp<-as.factor(fishmip$ssp)
-fishmip$mean<-fishmip$mean*100
-fishmip$sd<-fishmip$sd*100
-
-# test the plot
-# filtered_data<-subset(fishmip,
-#        EEZ %in% "Australia")
-# 
-# p <- ggplot(filtered_data, aes_string(x="year", y="mean",colour="ssp")) +
-#   geom_line(alpha=0.5) +
-#   geom_ribbon(aes(ymin=mean - sd, ymax=mean + sd,fill=ssp), alpha=0.2,color=NA)+
-#   # #theme(legend.position = "none") +
-#   theme_minimal() +
-#   ylab("Multimodel mean % change exploitable biomass")
-# 
-
+#THREDDS folder with the different climate forcings
+thredds_dir<-"http://portal.sf.utas.edu.au/thredds/dodsC/gem/fishmip/ISIMIP3a/InputData/climate/ocean/obsclim/global/monthly/historical/GFDL-MOM6-COBALT2/gfdl-mom6-cobalt2"
+#ESMname ="gfdl-mom6-cobalt2"
+modelscen = c("obsclim")
+varNames=c("chl","expc-bot","intpoc","intpp","intppdiat","intppdiaz","intpppico","mlotst-125","o2","o2-bot","o2-surf","ph","ph-bot","ph-surf","phyc","phyc-vint","phydiat","phydiat-vint","phydiaz","phydiaz-vint","phypico","phypico-vint","rsntds","siconc","so","so-bot","so-surf","thetao","thkcello","tob","tos","uo","vo","zmeso","zmeso-vint","zooc","zooc-vint")
+resOptions = c("60arcmin","15arcmin")
+filesuffix= "global_monthly_1961_2010.nc"
 
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      h2("FishMIP Ensemble EEZ Projections"),
-      selectInput(inputId = "EEZ", label = "EEZ",
-                  choices = levels(fishmip$EEZ),
-                  selected = "Australia"),
-           # selectInput(inputId = "model", "Model",
-           #         choices = levels(fishmip$model),
-           #         selected = "BOATS"),
-           #  selectInput(inputId = "esm", "CMIP6 Model",
-           #                    choices = levels(fishmip$esm),
-           #                     # multiple = TRUE,
-           #                     selected = c("gfdl-esm4")),
-      downloadButton(outputId = "download_data", label = "Download"),
+      h2("FishMIP Regional Model Climate Forcing Explorer"),
+      selectInput(inputId = "region", label = "Regional Model:",
+                  choices = regions,
+                  selected = "Prydz.Bay"),
+      
+      #to select the variable
+      selectInput("variable", "Variable:", choices=varNames,selected ="tos"),
+      
+      # select the resolution
+      selectInput(inputId = "res", "Resolution",
+                 choices = resOptions,
+                   selected = "60arcmin"),
+      
+     downloadButton(outputId = "download_data", label = "Download"),
     ),
     mainPanel(
-      plotlyOutput(outputId = "plot"), br(),
-      em("Postive and negative percentages indicate an increase and decrease from the historical reference period (mean 1990-1999)"),
-      br(), br(), br(),
-      DT::dataTableOutput(outputId = "table")
+      plotOutput(outputId = "plot1")
     )
   )
 )
 
 
 server <- function(input, output) {
-  filtered_data <- reactive({
-    subset(fishmip,
-           EEZ %in% input$EEZ)})
   
-  output$plot <- renderPlotly({
-    ggplotly({
-      p <- ggplot(filtered_data(), aes_string(x="year", y="median",colour="ssp")) +
-        geom_line(alpha=0.5) +
-        geom_ribbon(aes(ymin=min, ymax=max,fill=ssp), alpha=0.2,color=NA)+
-        # #theme(legend.position = "none") +
-        theme_minimal() +
-        ylab("Multimodel mean % change exploitable biomass")
+  filtered_mask <- reactive({
+    subset(mask,
+           region %in% input$region)})
 
-      p
-    })
+  output$plot1 <- renderPlot({
+    
+    mask_to_use <- st_as_sf(filtered_mask(), coords = c("Lon", "Lat"))
+    
+    file_to_get<-paste(thredds_dir,
+                       modelscen,
+                       variable=input$variable,
+                       res=input$res, filesuffix,sep="_")
+    
+    #terra::rast("http://portal.sf.utas.edu.au/thredds/fileServer/gem/fishmip/ISIMIP3a/InputData/climate/ocean/obsclim/global/monthly/historical/GFDL-MOM6-COBALT2/gfdl-mom6-cobalt2_obsclim_intppdiat_60arcmin_global_monthly_1961_2010.nc")
+    #nc<-terra::rast("Masks_netcdf_csv/gfdl-mom6-cobalt2_obsclim_intppdiat_60arcmin_global_monthly_1961_2010.nc")
+    # nc<-ncdf4::nc_open(file="Masks_netcdf_csv/gfdl-mom6-cobalt2_obsclim_intppdiat_60arcmin_global_monthly_1961_2010.nc",return_on_error=TRUE)
+    # file_to_get<-"Masks_netcdf_csv/gfdl-mom6-cobalt2_obsclim_intppdiat_60arcmin_global_monthly_1961_2010.nc"
+    #file_to_get<-"http://portal.sf.utas.edu.au/thredds/dodsC/gem/fishmip/ISIMIP3a/InputData/climate/ocean/obsclim/global/monthly/historical/GFDL-MOM6-COBALT2/gfdl-mom6-cobalt2_obsclim_zmicro-vint_15arcmin_global_monthly_1961_2010.nc"
+    gridded_ts <- brick(file.path(file_to_get))
+    # # cut selected region out
+    crs(gridded_ts) = crs(mask_to_use)
+    temp <- crop(gridded_ts, extent(mask_to_use))
+    gridded_ts <- mask(temp, mask_to_use)
+    
+    # gridded_ts_df <- as.data.frame(gridded_ts, xy = TRUE)
+    # names(gridded_ts_df)<-c("lon","lat","var")
+    #  
+    # mean <- gridded_ts_df %>%
+    # group_by(lat, lon) %>% 
+    # summarise(mean = mean(var, na.rm = F))
+   
+    
+    
+    twoplots <- function(dat) {
+      
+      timevals<-720:1319
+      par(mfrow = c(2,1), mai=c(rep(0.6,4)))               # This sets the new parameters: 2 columns (plots)
+      
+      # Plotting the first
+      with(dat,
+           plot(mean(dat))
+      )
+      
+      # Plotting the second
+      with(dat,
+           plot(timevals,cellStats(dat*(area(dat)), 'mean'),typ="l",ylab=input$variable)
+      )
+      
+      
+    }  
+    
+   
+  twoplots(dat=gridded_ts)
+   # calculate spatially weighthed average of variables selected
+   # ts<-cellStats(gridded_ts*(area(gridded_ts)), 'mean') 
+   # timevals<-720:1319
+   # plot(timevals,ts,typ="l")
+    
+     # ggplotly({
+     #  plot<-ggplot() +
+     #     geom_raster(data = mean , aes(x = lon, y = lat, fill = var)) +
+     #     scale_fill_viridis_c() +
+     #     coord_quickmap()
+     #  plot
+     #  })
+    
+    
   })
   
-  output$table <- DT::renderDataTable({
-    filtered_data()
-  })
   
-  output$download_data <- downloadHandler(
-    filename = "download_data.csv",
-    content = function(file) {
-      data <- filtered_data()
-      write.csv(data, file, row.names = FALSE)
-    }
-  )
+  # output$plot2 <- renderPlot({
+  # 
+  # 
+  #   
+  # })
+  
+  # output$table <- DT::renderDataTable({
+  #   gridded_ts_df <- as.data.frame(gridded_ts, xy = TRUE)
+  #   gridded_ts_df
+  # })
+  # output$download_data <- downloadHandler(
+  #   filename = "download_data.csv",
+  #   content = function(file) {
+  #     data <- filtered_data()
+  #     write.csv(data, file, row.names = FALSE)
+  #   }
+  # )
   
 }
 
