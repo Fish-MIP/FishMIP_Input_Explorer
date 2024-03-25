a4fa22fdeeb4ca2d802d70c6cc2d34a3c1be7c96
lidefi87 <lidefi87@hotmail.com>
2024-03-25 06:21:10 +0000

    using simplified files as shiny app inputs

--- app.R
+++ app.R
@@ -22,17 +22,17 @@ area_ras <- rast("/rd/gem/private/users/ldfierro/FishMIP_regions/ESM_Sample_Data
 # #Get list of variables with four dimensions (lon, lat, time and depth)
 # four_dim_mods <- read_csv("Masks_netcdf_csv/four_dimensional_rasters.csv") |> 
 #   pull(vars)
-# #Get list of depth bins in four dimensional variables
-# depth_bins <- read_csv("Masks_netcdf_csv/depth_layers.csv") |> 
-#   pull(depths)
 
-# Base folder containing Earth System Model (ESM) data
-base_dir <- "/rd/gem/public/fishmip/ISIMIP3a/InputData/climate/ocean/obsclim/regional/monthly/historical/GFDL-MOM6-COBALT2"
+# Folders containing Earth System Model (ESM) data
+download_dir <- "/rd/gem/public/fishmip/ISIMIP3a/InputData/climate/ocean/obsclim/regional/monthly/historical/GFDL-MOM6-COBALT2/smaller/"
+maps_dir <- "/rd/gem/public/fishmip/ISIMIP3a/InputData/climate/ocean/obsclim/regional/monthly/historical/GFDL-MOM6-COBALT2/maps_data/"
+# ts_dir <- "/rd/gem/public/fishmip/ISIMIP3a/InputData/climate/ocean/obsclim/regional/monthly/historical/GFDL-MOM6-COBALT2/ts_data/"
+
 # Getting list of all files within folder
-data_files <- list.files(base_dir, pattern = "15arcmin") |> 
-  str_subset("phydiat_depth", negate = T)
+var_files <- list.files(maps_dir, pattern = "15arcmin") #|> 
+  # str_subset("phydiat_depth|chl", negate = T)
 #Getting names of environmental variables available
-varNames <- str_extract(data_files, ".*obsclim_(.*)_[0-9]{2}arc.*", group = 1) |> 
+varNames <- str_extract(var_files, ".*obsclim_(.*)_[0-9]{2}arc.*", group = 1) |> 
   unique()
 
 
@@ -62,7 +62,7 @@ ui <- fluidPage(
                   label = NULL,
                   choices = keys$region,
                   selected = "Prydz Bay"),
-      p("2. Choose an environmental variable"),
+      p("2. Choose an environmental variable from GFDL-MOM6"),
       #to select the variable
       selectInput(inputId = "variable", 
                   label = NULL, 
@@ -105,57 +105,45 @@ server <- function(input, output, session) {
     #Merge region and variable prior to identifying correct file
     sub <- str_c(name_var, "_15arcmin_", name_reg)
     #Identifying correct file
-    reg_files <- str_subset(data_files, sub)
+    file_path <- str_subset(var_files, sub)
   })
   
   #Loading dataset
   var_fishmip <- reactive({
     #Get full file path to relevant file
-    env_file <- file.path(base_dir, region_fishmip())
+    map_file <- file.path(maps_dir, region_fishmip())
     #Load file
-    df <- read_csv(env_file)
+    df <- read_csv(map_file)
     #Getting units from dataset
     unit <- df |> 
-      select(units) |> 
+      distinct(units) |> 
       drop_na() |> 
       pull()
     #Getting standard name from dataset
     std_name <- df |> 
-      select(standard_name) |> 
+      distinct(standard_name) |> 
       drop_na() |>
       pull() |> 
       str_replace_all("_", " ") |> 
       str_to_sentence()
     #Creating label for figures
     cb_lab <- paste0(input$variable, " (", unit, ")")
-    #Selecting relevant data frame columns
-    df <- df |> 
-      #Metadata not included in data frame used for plots
-      select(c(lat, lon, matches("[0-9]{4}"))) |> 
-      #Reorganise data
-      pivot_longer(!c(lat, lon), names_to = "time", values_to = "vals") |> 
-      #Remove rows with NA values
-      drop_na() |> 
-      #Change time column to date format
-      mutate(time = my(time)) |> 
-      #Arrange dataset by time column
-      arrange(time)
-    df$standard_name <- std_name
-    df$units <- unit
+    
+    #Get full file path to relevant file
+    # ts_file <- file.path(ts_dir, region_fishmip())
+    #Load file
+    # df2 <- read_csv(ts_file)
+    
     #Return data frame
-    return(list(data = df,
+    return(list(map_data = df,
+                # ts_data = df2,
+                std_name = std_name,
                 fig_label = cb_lab))
   })
   
   # Creating first plot
   output$plot1 <- renderPlot({
-    clim <- var_fishmip()$data |> 
-      group_by(lon, lat) |>
-      summarise(vals = mean(vals))
-    
-    #Creating plot
-    clim |> 
-      drop_na(vals) |> 
+    var_fishmip()$map_data |> 
       ggplot(aes(x = lon, y = lat, fill = vals)) +
       geom_raster()+
       scale_fill_viridis_c(guide = guide_colorbar(ticks.linewidth = 0.75,
@@ -164,43 +152,46 @@ server <- function(input, output, session) {
                            na.value = NA) +
       guides(fill = guide_colorbar(title = var_fishmip()$fig_label, title.position = "top", 
                                    title.hjust = 0.5))+
-      labs(title = var_fishmip()$data$standard_name, 
+      labs(title = var_fishmip()$std_name, 
            x = "Longitude", y = "Latitude")+
       theme_classic()+
       theme(legend.position = "bottom", legend.key.width = unit(2, "cm"),
             plot.title = element_text(hjust = 0.5))
-  
   })
 
    output$plot2 <- renderPlot({
      # calculate spatially weighted average of variables selected
-     ts <- var_fishmip()$data |> 
-       left_join(area_ras, by = join_by(lon, lat)) |> 
-       group_by(time) |> 
-       summarise(vals = weighted.mean(vals, area_m, na.rm = T))
-     
-     #Plot weighted means
-     ggplot(ts, aes(x = time, y = vals)) +
-       geom_line() +
-       geom_smooth(colour = "steelblue")+
-       theme_classic() +
-       scale_x_date(date_labels = "%b-%Y", date_breaks = "18 months")+
-       labs(title = var_fishmip()$data$standard_name, 
-            y = var_fishmip()$fig_label)+
-       theme(axis.text = element_text(size = 12),
-             axis.text.x = element_text(angle = 90, vjust = 0.5),
-             axis.title.y = element_text(size = 12),
-             axis.title.x = element_blank(),
-             plot.title = element_text(hjust = 0.5))
-   })
+     # var_fishmip()$ts_data |> 
+     #   #Plot weighted means
+     #   ggplot(aes(x = date, y = vals)) +
+     #     geom_line() +
+     #     geom_smooth(colour = "steelblue")+
+     #     theme_classic() +
+     #     scale_x_date(date_labels = "%b-%Y", date_breaks = "18 months")+
+     #     labs(title = var_fishmip()$std_name, 
+     #          y = var_fishmip()$fig_label)+
+     #     theme(axis.text = element_text(size = 12),
+     #           axis.text.x = element_text(angle = 90, vjust = 0.5),
+     #           axis.title.y = element_text(size = 12),
+     #           axis.title.x = element_blank(),
+     #           plot.title = element_text(hjust = 0.5))
+     })
 
+   #Loading download dataset
+   down_fishmip <- reactive({
+     #Get full file path to relevant file
+     down_file <- file.path(download_dir, region_fishmip())
+     #Load file
+     down_df <- read_csv(down_file)
+   })
+   
    output$download_data <- downloadHandler(
      filename = function(){
        region_fishmip()
      },
      #Creating name of download file based on original file name
      content = function(file){
-       write_csv(var_fishmip()$data, file)
+       write_csv(down_fishmip(), file)
      }
    )
   
