dcfa7432621c1c598d7cff8b3995f4fefc6eef64
Tormey Reimer <tormey.reimer@utas.edu.au>
2024-08-27 09:50:58 +1000

    Script for programatically downloading WOA data

--- data_wrangling/downloading_WOA.R
+++ data_wrangling/downloading_WOA.R
@@ -1,23 +1,50 @@
+###########################################################################
+#
+# Programmatically downloading data from WOA
+# Author: Tormey Reimer
+# Date: 2024-08-27
+# This script downloads temperature and salinity data from the World Ocean 
+# Atlas thredds server. All the files downloaded are:
+# * at the 0.25 degree grid resolution
+# * the average of seven decadal means from 1955 to 2022 (decav)
+# * one file per month (01-12)
+# Documentation here: https://www.ncei.noaa.gov/data/oceans/woa/WOA23/DOCUMENTATION/WOA23_Product_Documentation.pdf
+#
+###########################################################################
+
 library(thredds)
 library(curl)
-library(stringr)
+library(tidyverse)
 
-#Defining domain name for THREDDS server
-domain <- "https://www.ncei.noaa.gov/thredds-ocean/catalog/woa23/DATA/temperature/netcdf/decav/0.25/catalog.xml"
-
-get_thredds <- function(domain, destdir, code){
-  noaa_reg <- CatalogNode$new(domain, prefix = "thredds")
+# Function to download the correct file from the thredds server
+get_thredds <- function(domain, catalogue, destdir, month){
+  noaa_reg <- CatalogNode$new(catalogue, prefix = "thredds")
   base_http <- noaa_reg$list_services()$http[["base"]]
 
   file <- noaa_reg$list_datasets()
-  file <- file[str_extract(names(file), "t(\\d{2})", group = 1) %in% code]
+  file <- file[str_extract(names(file), "t(\\d{2})", group = 1) == month]
   name <- names(file)
   
-  curl_download(paste0("https://www.ncei.noaa.gov/", 
-                       base_http, 
-                       noaa_reg$get_datasets()[[names(file)]]$url), 
+  curl_download(paste0(domain, base_http, noaa_reg$get_datasets()[[names(file)]]$url), 
                 destfile = paste0(destdir, name))
 }
 
-get_thredds(destdir = "example_data/", domain = domain, code = "02")
+# If you source this script it might take a while
+start <- Sys.time()
+print(paste0("Started downloads at ", start))
 
+# Apply the function to all the desired files
+sapply(X = c("02", "03"), 
+       FUN = get_thredds, 
+       domain = "https://www.ncei.noaa.gov/",
+       catalogue = paste0(domain, "thredds-ocean/catalog/woa23/DATA/temperature/netcdf/decav/0.25/catalog.xml"),
+       destdir = "example_data/")
+
+sapply(X = c("02", "03"), 
+       FUN = get_thredds, 
+       domain = "https://www.ncei.noaa.gov/",
+       catalogue = paste0(domain, "thredds-ocean/catalog/woa23/DATA/salinity/netcdf/decav/0.25/catalog.xml"),
+       destdir = "example_data/")
+
+end <- Sys.time()
+print(paste0("Finished downloads at ", end, ". Total time taken: ", round(difftime(end, start, units='mins'), 2), " minutes."))
