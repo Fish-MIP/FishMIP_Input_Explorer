1d8642a54f9bb7e07a20ad208ab52829d5cdd948
Tormey Reimer <tormey.reimer@utas.edu.au>
2024-08-26 20:27:41 +1000

    Turned into function

--- data_wrangling/downloading_WOA.R
+++ data_wrangling/downloading_WOA.R
@@ -4,9 +4,20 @@ library(stringr)
 
 #Defining domain name for THREDDS server
 domain <- "https://www.ncei.noaa.gov/thredds-ocean/catalog/woa23/DATA/temperature/netcdf/decav/0.25/catalog.xml"
-noaa_reg <- CatalogNode$new(domain, prefix = "thredds")
-seasonal_temp <- noaa_reg$list_datasets()
-seasonal_temp <- seasonal_temp[str_extract(names(seasonal_temp), "t(\\d{2})", group = 1) %in% 13:16]
-base_http <- noaa_reg$list_services()$http[["base"]]
-noaa_reg$get_dataset_names()
-curl_download(paste0(base_http, noaa_reg$get_datasets()[[names(seasonal_temp)[1]]]$url))
+
+get_thredds <- function(domain, destdir, code){
+  noaa_reg <- CatalogNode$new(domain, prefix = "thredds")
+  base_http <- noaa_reg$list_services()$http[["base"]]
+
+  file <- noaa_reg$list_datasets()
+  file <- file[str_extract(names(file), "t(\\d{2})", group = 1) %in% code]
+  name <- names(file)
+  
+  curl_download(paste0("https://www.ncei.noaa.gov/", 
+                       base_http, 
+                       noaa_reg$get_datasets()[[names(file)]]$url), 
+                destfile = paste0(destdir, name))
+}
+
+get_thredds(destdir = "example_data/", domain = domain, code = "02")
+
