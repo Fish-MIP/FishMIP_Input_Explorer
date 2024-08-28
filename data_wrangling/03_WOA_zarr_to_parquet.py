621f68000b1043d7a3882db11b38563e771eb161
Tormey Reimer <tormeyreimer@gmail.com>
2024-08-28 13:59:44 +1000

    Pre-filtering parquets does not reduce size

--- data_wrangling/03_WOA_zarr_to_parquet.py
+++ data_wrangling/03_WOA_zarr_to_parquet.py
@@ -14,6 +14,7 @@ import datetime as dt
 import dask
 import os
 import zarr
+import math
 
 WOA_path = "example_data/WOA_data/"
 
@@ -28,18 +29,25 @@ df_temp = xarray.open_zarr(WOA_path + "WOA_temperature.zarr")
 #     group.to_parquet(WOA_path + '/' + 'WOA_temperature_decav_monthly_' + f'{dep}.parquet', 
 #                      mode = 'w+') # Overwrites existing files
 
-# For grouping by region and saving
+# For grouping by region and saving.
+# Some lines are commented out because removing Nan values here does not make the files smaller.
 for i in range(36):
     df_1 = df_temp.where(
-        (df.iloc[i].xmin < df_temp.lon) & (df_temp.lon < df.iloc[i].xmax) & 
-        (df.iloc[i].ymin < df_temp.lat) & (df_temp.lat < df.iloc[i].ymax), 
+        (df.iloc[i].xmin < df_temp.lon) & (df_temp.lon < df.iloc[i].xmax) &
+        (df.iloc[i].ymin < df_temp.lat) & (df_temp.lat < df.iloc[i].ymax),
         drop=True
     ).to_dataframe()
-    filename = f'{df.iloc[i].region.replace("i'i", "ii")}.parquet'
-    filename = f'{df.iloc[i].region.replace(" ", "_")}.parquet'
-    df_1.to_parquet(WOA_path + 'WOA_temperature_decav81B0_monthly_' + filename) # Overwrites existing files
+    # df_1 = df_1.reset_index()
+    df_1 = pd.melt(df_1, id_vars = ['time', 'depth', 'lat', 'lon'], value_vars = 't_an', var_name = 'variable', value_name = 'value')
+    # df_1 = df_1.set_index(['time', 'depth', 'lat', 'lon'])
+    # df_2 = df_1[df_1['value'].notnull()]
+    filename = f'{df.iloc[i].region.replace("i'i", "ii")}'
+    filename = f'{df.iloc[i].region.replace(" ", "_")}'
+    df_1.to_parquet(WOA_path + 'WOA_temperature_decav81B0_monthly_' + filename + '.parquet') # Overwrites existing files
+    # df_2.to_parquet(WOA_path + 'WOA_temperature_decav81B0_monthly_' + filename + '_NoNans.parquet') # Overwrites existing files
 
-df_temp = xarray.open_zarr("example_data/WOA_data/WOA_salinity.zarr")
+
+df_temp = xr.open_zarr("example_data/WOA_data/WOA_salinity.zarr")
 
 # Save by region
 for i in range(36):
@@ -48,6 +56,12 @@ for i in range(36):
         (df.iloc[i].ymin < df_temp.lat) & (df_temp.lat < df.iloc[i].ymax),
         drop=True
     ).to_dataframe()
-    filename = f'{df.iloc[i].region.replace("i'i", "ii")}.parquet'
-    filename = f'{df.iloc[i].region.replace(" ", "_")}.parquet'
-    df_1.to_parquet(WOA_path + 'WOA_salinity_decav81B0_monthly_' + filename) # Overwrites existing files
+    # df_1 = df_1.reset_index()
+    df_1 = pd.melt(df_1, id_vars = ['time', 'depth', 'lat', 'lon'], value_vars = 's_an', var_name = 'variable', value_name = 'value')
+    # df_1 = df_1.set_index(['time', 'depth', 'lat', 'lon'])
+    # df_2 = df_1[df_1['value'].notnull()]
+    filename = f'{df.iloc[i].region.replace("i'i", "ii")}'
+    filename = f'{df.iloc[i].region.replace(" ", "_")}'
+    df_1.to_parquet(WOA_path + 'WOA_salinity_decav81B0_monthly_' + filename + '.parquet') # Overwrites existing files
+    # df_2.to_parquet(WOA_path + 'WOA_salinity_decav81B0_monthly_' + filename + '_NoNans.parquet') # Overwrites existing files
+
