3b2951a02c4dd23473606b4402a595daa20ca2dc
lidefi87 <lidefi87@hotmail.com>
2024-09-04 12:52:28 +1000

    creating function to avoid duplicating code

--- data_wrangling/02_WOA_ncs_to_zarr.py
+++ data_wrangling/02_WOA_ncs_to_zarr.py
@@ -3,62 +3,66 @@
 # Converting downloaded WOA data to useable format
 # Author: Tormey Reimer
 # Date: 2024-08-27
+# Edited by: Denisse Fierro Arcos
+# Edited on: 2024-09-04
+# 
 # This script consolidates the data already downloaded from WOA by the
 # downloading_WOA.R script and converts it into cloud-friendly zarr files.
 #
 ###########################################################################
 
-# Setting up
+# Calling relevant libraries
 import xarray as xr
+import zarr
+from dask.distributed import Client
 import pandas as pd
 import datetime as dt
-import glob
-import dask
+from glob import glob
 import os
-import netCDF4 as nc
-import zarr
-#from dask.distributed import Client
-#client = Client(threads_per_worker=1)
 
-def get_absolute_paths(filenames, directory):
-    absolute_paths = []
-    for filename in filenames:
-        absolute_path = os.path.abspath(os.path.join(directory, filename))
-        absolute_paths.append(absolute_path)
-    return absolute_paths
+# Starting a cluster
+client = Client(threads_per_worker = 1)
 
-# Get all files within the WOA_data directory
-WOA_path = "example_data/WOA_data/"
-WOA_files = get_absolute_paths(os.listdir(WOA_path), WOA_path)
+# Defining function to save netCDF files to zarr files
+def netcdf_to_zarr(list_files, var_name, file_out):
+    '''
+    Inputs:
+    list_files (character list) Full paths to WOA files that need to be 
+    merged and save as a single zarr collection. All files must contain 
+    the same variable of interest.
+    var_name (character) Name of variable to be saved as zarr file.
+    file_out (character) Full file path where zarr file will be saved.
 
-# Seperate by variable
-WOA_temp = [k for k in WOA_files if 'decav81B0_t' in k]
-WOA_sali = [k for k in WOA_files if 'decav81B0_s' in k]
+    Outputs:
+    None. This function saves a zarr file to disk.
+    '''
+    da = xr.open_mfdataset(list_files, decode_times = False)[var_name]
+    
+    # Fix time coordinate variable - Keep name of month only
+    units, reference_date = da.time.attrs['units'].split('since')
+    da['time'] = pd.date_range(start = reference_date, periods = da.sizes['time'], 
+                               freq = 'MS').strftime('%B')
+    da = da.rename({'time': 'month'})
+    
+    #Rechunk data 
+    da_rechunk = da.chunk({'month': 12, 'depth': 57, 'lat': 120, 'lon': 240})
 
-# Open and combine files
-temp = xr.open_mfdataset(WOA_temp, engine = "netcdf4", decode_times = False)
+    #Save data, but ensure directory exists
+    out_dir = os.path.dirname(file_out)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    da_rechunk.to_zarr(file_out, consolidated = True, mode = 'w')
 
-# Fix time coordinate variable
-units, reference_date = temp.time.attrs['units'].split('since')
-temp['time'] = pd.date_range(start = reference_date, periods = temp.sizes['time'], freq='MS')
+# Defining path to WOA_data directory
+WOA_path = '/g/data/vf71/WOA_data/'
 
-# Isolate mean decadal monthly temperature
-df_temp = temp['t_an']
-df_temp = df_temp.chunk({'time': 12, 'depth': 57, 'lat': 50, 'lon': 100})
-df_temp.to_zarr("example_data/WOA_data/WOA_temperature.zarr", mode="w")
+# Save WOA temperature files as zarr files
+WOA_temp = sorted(glob(os.path.join(WOA_path, '*/*_t*.nc')))
+temp_out = os.path.join(WOA_path, 'woa23_month_clim_mean_temp_1981-2010.zarr')
+netcdf_to_zarr(WOA_temp, 't_an', temp_out)
 
-# Clear temp variable to free memory
-temp = []
-
-# Open and combine salinity files
-temp = xr.open_mfdataset(WOA_sali, engine = "netcdf4", decode_times = False)
-
-# Fix time coordinate variable
-units, reference_date = temp.time.attrs['units'].split('since')
-temp['time'] = pd.date_range(start = reference_date, periods = temp.sizes['time'], freq='MS')
-
-# Isolate mean decadal monthly temperature
-df_temp = temp['s_an']
-df_temp = df_temp.chunk({'time': 12, 'depth': 57, 'lat': 50, 'lon': 100})
-df_temp.to_zarr("example_data/WOA_data/WOA_salinity.zarr", mode="w")
+# Save WOA salinity files as zarr files
+WOA_sal = sorted(glob(os.path.join(WOA_path, '*/*_s*.nc')))
+sal_out = os.path.join(WOA_path, 'woa23_month_clim_mean_sal_1981-2010.zarr')
+netcdf_to_zarr(WOA_sal, 's_an', sal_out)
 
