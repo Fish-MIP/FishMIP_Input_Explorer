ff172b7b7aa8571cb0988655d569a206e99b7b91
lidefi87 <lidefi87@hotmail.com>
2024-09-05 17:15:22 +1000

    processing overall climatology files for maps

--- data_wrangling/03_WOA_zarr_data_extraction.py
+++ data_wrangling/03_WOA_zarr_data_extraction.py
@@ -42,10 +42,14 @@ def mask_boolean_ard_data(file_path, boolean_mask_ras):
     da = xr.open_zarr(file_path)
     [var] = list(da.keys())
     da = da[var]
-    da = da.chunk({'month': 12, 'depth': 57, 'lat': lat_chunk, 'lon': lon_chunk})
-    #Make sure months are in correct format
-    da['month'] = pd.date_range(start = '2010-01-01', periods = 12, 
-                                freq = 'MS').strftime('%B')
+    #Ensure chunks are the same as mask
+    if 'month' in da.dims:
+        da = da.chunk({'month': 12, 'depth': 57, 'lat': lat_chunk, 'lon': lon_chunk})
+        #Make sure months are in correct format
+        da['month'] = pd.date_range(start = '2010-01-01', periods = 12, 
+                                    freq = 'MS').strftime('%B')
+    else:
+        da = da.chunk({'depth': 57, 'lat': lat_chunk, 'lon': lon_chunk})
         
     #Apply mask for all regions to decrease dataset size
     da = da.where(boolean_mask_ras == 1)
@@ -79,19 +83,28 @@ def mask_ard_data(ard_da, shp_mask, file_out):
     da_mask.encoding = {}
 
     #Check file extension included in path to save data
-    for i, c in enumerate(da_mask.chunks):
-        if len(c) > 1 and len(set(c)) > 1:
-            print(f'Rechunking {file_out}.')
-            print(f'Dimension "{da_mask.dims[i]}" has unequal chunks.')
-            da_mask = da_mask.chunk({da_mask.dims[i]: '200MB'})
-    da_mask.to_zarr(file_out, consolidated = True, mode = 'w')
-
-# Defining path to WOA_data directory
-WOA_zarr = glob('/g/data/vf71/WOA_data/global/*.zarr')
-
-#Define (or create) folder for outputs
-base_out = '/g/data/vf71/WOA_data/regional'
-os.makedirs(base_out, exist_ok = True)
+    if file_out.endswith('zarr'):
+        for i, c in enumerate(da_mask.chunks):
+            if len(c) > 1 and len(set(c)) > 1:
+                print(f'Rechunking {file_out}.')
+                print(f'Dimension "{da_mask.dims[i]}" has unequal chunks.')
+                da_mask = da_mask.chunk({da_mask.dims[i]: '200MB'})
+        da_mask.to_zarr(file_out, consolidated = True, mode = 'w')
+    if file_out.endswith('parquet'):
+        #Keep data array attributes to be recorded in final data frame
+        da_attrs = ard_da.attrs
+        da_attrs = pd.DataFrame([da_attrs])
+        ind_wider = ['lat', 'lon', 'depth', 'vals']
+        #Turn extracted data into data frame and remove rows with NA values
+        df = da_mask.to_series().to_frame().reset_index().dropna()
+        #Changing column name to standardise across variables
+        df = df.rename(columns = {ard_da.name: 'vals'}).reset_index(drop = True)
+        #Reorganise data
+        df = df[ind_wider]
+        #Include original dataset attributes
+        df = pd.concat([df, da_attrs], axis = 1)
+        #Saving data frame
+        df.to_parquet(file_out)
 
 #Loading FishMIP regional models shapefile
 rmes = gpd.read_file('/g/data/vf71/shared_resources/FishMIP_regional_models/FishMIP_regional_models.shp')
@@ -102,13 +115,27 @@ mask_ras = xr.open_dataarray(os.path.join('/g/data/vf71/shared_resources/FishMIP
 #Rechunking data to make it more manageable
 mask_ras = mask_ras.chunk({'lat': 144, 'lon': 288})
 
+# Defining path to WOA_data directory
+WOA_zarr = glob('/g/data/vf71/WOA_data/global/*.zarr')
+
+#Define (or create) folders where outputs will be stored
+base_out_clim = '/g/data/vf71/WOA_data/regional/climatology'
+os.makedirs(base_out_clim, exist_ok = True)
+base_out_month = '/g/data/vf71/WOA_data/regional/monthly'
+os.makedirs(base_out_month, exist_ok = True)
+
 #Applying functions to WOA files
 for f in WOA_zarr:
     #Open data array as ARD
-    da = mask_boolean_ard_data(f, mask_ras)
-        
-    #Adding output folder to create full file path
-    full_file_out = os.path.join(base_out, os.path.basename(f))
+    da = mask_boolean_ard_data(f, mask_ras)   
+    
+    #Create full file path
+    if 'month' in f:
+         #Adding output folder to create full file path
+        full_file_out = os.path.join(base_out_month, os.path.basename(f))
+    else:
+        full_file_out = os.path.join(base_out_clim,
+                                     os.path.basename(f).replace('zarr', 'parquet'))
 
     #Extract data for each region included in the regional mask
     for i in rmes.region:
