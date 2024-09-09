f49cc3586b5349fd7ac78eb585cb14d3b2ae28c7
lidefi87 <lidefi87@hotmail.com>
2024-09-09 12:55:59 +1000

    calculating timeseries and climatologies for 2d gfdl data

--- R/merging_woa_gfdl_metadata.R
+++ R/merging_woa_gfdl_metadata.R
@@ -1,5 +1,9 @@
 # Merging GFDL and WOA metadata -----
-# These metadata files were produced using scripts available at:
+#
+# Author: Denisse Fierro Arcos
+# Date: 2024-09-09
+#
+# Metadata files for GFDL and WOA data were produced using scripts available at:
 # https://github.com/Fish-MIP/FishMIP_regions/blob/main/scripts/05_compiling_GFDL_metadata.ipynb
 # https://github.com/Fish-MIP/processing_WOA_data/blob/main/scripts/04P_compiling_WOA_metadata.ipynb
 
@@ -14,9 +18,10 @@ woa_meta <- read_csv("/g/data/vf71/WOA_data/woa_var_keys.csv",
   rename(woa_name = short_name) |> 
   mutate(woa_name_code = case_when(woa_name == "t_an" ~ "temp",
                                     woa_name == "s_an" ~ "sal"),
-         standard_name = str_to_title(str_replace_all(standard_name, "_", " ")))
+         standard_name = str_to_title(str_replace_all(standard_name, "_", " ")),
+         units = case_when(str_detect(units, "degrees") ~ "°C",
+                           T ~ units))
 
-## Loading GFDL metadata and merging with WOA metadata ----
 all_meta <- file.path("/g/data/vf71/fishmip_inputs/ISIMIP3a/global_inputs", 
                        "obsclim/gfdl_var_keys.csv") |> 
   read_csv(show_col_types = FALSE) |> 
@@ -24,7 +29,9 @@ all_meta <- file.path("/g/data/vf71/fishmip_inputs/ISIMIP3a/global_inputs",
   select(gfdl_name:units) |> 
   mutate(woa_name = case_when(str_detect(standard_name, "temperature") ~ "t_an",
                               str_detect(standard_name, "salinity") ~ "s_an",
-                              T ~ NA)) |> 
+                              T ~ NA),
+         units = case_when(str_detect(units, "degC") ~ "°C",
+                           T~ units)) |> 
   left_join(woa_meta, by = join_by(woa_name), suffix = c(".gfdl", ".woa"))
 
 # Saving result ----
